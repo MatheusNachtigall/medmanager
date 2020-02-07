536efd44c4862e3451fe8ac949d9e505c85d703b
Matheus Nachtigall <matheustche@gmail.com>
2020-02-07 17:41:36 -0300

    .

--- trunk/UnitedToWin/www/js/app.js
+++ trunk/UnitedToWin/www/js/app.js
@@ -1,22 +1,25 @@
-
-
 $(document).ready(function(){
     $('select').formSelect();
     $('.tabs').tabs();
+    $('.sidenav').sidenav({
+        draggable: true
+    });
+    $(".dropdown-trigger").dropdown({
+        hover: false
+    });
+    $('.collapsible').collapsible();
+    $('.date').mask('00/00/0000', { placeholder: "__/__/____" });
+    $('.money').mask('#.##0,00', { reverse: true });
 });
 
 
 $("#btn-limpar").on("click", function () {
-    $('#codMaca').val('');
-    $('#codAlmas').val('');
-    $('#nivel').val('XX');
-    $('input[name="mult"][value="1"]').prop("checked",true)
-    $("#respPortal").text("")
-    $("#respPortalVezes").text("")
-    
-    $('#nvAtual').val('');
-    $('#nvDesejado').val('');
-    $("#respTreinamento").text("")
-    $("#respDiamantes").text("")
+    $('#hospital').val("");
+    $( 'select' ).formSelect();
+    $('#data_plantao').val('');
+    $('#data_pagamento').val('');
+    $('#data_plantao').val('');
+    $('input[type="checkbox"]').prop("checked",false)
+    $('#valor').val('');
 
 });
